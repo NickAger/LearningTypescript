62dbe36c0cea0da1a5b224e758e75e3463424362
Nick Ager <nick.ager@gmail.com>
2020-09-08 16:04:30 +0100

    Basic application working with `FunctionComponent` oddness with setState when using objects that I want to try to understand.

--- EssentialTypescript/Chapter19/reactapp/src/App.tsx
+++ EssentialTypescript/Chapter19/reactapp/src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, {Component} from 'react';
+import React, {FunctionComponent, useState} from "react"
+import {Product, Order} from './data/entities'
+import {ProductList} from './productList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let testData: Product[] = [1, 2, 3, 4, 5].map(num => ({
+  id: num, name: `Prod${num}`, category: `Cat${num %2}`,
+  description: `Product ${num}`, price: 100 + (num * 10)}))
+
+interface Props {
+  // no props required
 }
+
+const App: FunctionComponent<Props> = () => {
+  const [order, setOrder] = useState<Order>(new Order())
+
+      return <div className="App">
+      <ProductList products={testData}
+        categories={[...new Set(testData.map(p=>p.category))]}
+        order={order}
+        addToOrder={(product: Product, quantity: number) => {
+          order.addProduct(product, quantity)
+          let orderCopy = order.makeACopy()
+          setOrder(orderCopy)
+        }}
+      />
+    </div>
+}
+
+// class version
+
+// interface Props {
+//   // no props required
+// }
+
+// interface State {
+//   order: Order
+// }
+
+// class App extends Component<Props, State> {
+//   constructor(props: Props) {
+//     super(props)
+//     this.state = {
+//       order: new Order()
+//     }
+//   }
+
+//   render = () => 
+//       <div className="App">
+//        <ProductList products={testData}
+//         categories={[...new Set(testData.map(p=>p.category))]}
+//         order={this.state.order}
+//         addToOrder={this.addToOrder}
+//        />
+//       </div>
+
+//   addToOrder = (product: Product, quantity: number) => {
+//     this.state.order.addProduct(product, quantity)
+//     this.setState(this.state)
+//   }
+// }
 
 export default App;
--- EssentialTypescript/Chapter19/reactapp/src/header.tsx
+++ EssentialTypescript/Chapter19/reactapp/src/header.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const Header: FunctionComponent<Props> = (props) => {
     let count = props.order.productCount
     return <div className="p-1 bg-secondary text-white text-right">
-        { count === 0 ? "(No Selection" : `${count} product(s), $${props.order.total.toFixed(2)}`}
+        { count === 0 ? "(No Selection)" : `${count} product(s), $${props.order.total.toFixed(2)}`}
         <button className="btn btn-sm btn-primary m-1">
             Submit Order
         </button>
